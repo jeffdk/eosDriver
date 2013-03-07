85dcae97e287130e7eaad2c51c10bcc3ed2e1652
Christian D. Ott <cott@tapir.caltech.edu>
2013-03-07 05:21:29 -0800

    * update tov test

--- tovtoy.py
+++ tovtoy.py
@@ -13,14 +13,14 @@ myeos = eosDriver('LS220_234r_136t_50y_analmu_20091212_SVNr26.h5')
 tovinfo = tovinfoclass()
 tovinfo.polyK = 100.0
 tovinfo.polyG = 2.0
-tovinfo.nzones = 40000
+tovinfo.nzones = 80000
 tovinfo.rmax = 50.0
 tovinfo.eostype = 3
 
-mytype = "fixed_ye_temp"
+mytype = "fixed_ye_entropy"
 par1 = 0.5
 par2 = 0.1
-rhomin = 1.0e6
+rhomin = 1.0e7
 rhomax = 8.0e15
 tovinfo.eoslrhomin = log10(rhomin*rho_gf)
 tovinfo.eoslrhomax = log10(rhomax*rho_gf)
@@ -36,6 +36,10 @@ tovinfo.eosdlrhoi = 1.0/dlrho
 rho_c = 4.717563e-04
 rho_c = 2.0e14*rho_gf
 rho_c = 8.615461e-04
+rho_c = 4.0e14*rho_gf
+rho_c = 6.864997E-04
+rho_c = 7.705537E-3
+rho_c = 1.258022215E+15*rho_gf
 print rho_c*inv_rho_gf
 (tovout,isurf,rad,dr) = tov_integrate(rho_c,tovinfo)
 
@@ -43,6 +47,7 @@ print rho_c*inv_rho_gf
 print "%4d %15.6E %15.6E %15.6E %15.6E" % \
     (isurf,rho_c,tovout[isurf,3],tovout[isurf,4],rad[isurf]*inv_length_gf)
 
+print rho_c*inv_rho_gf, tovout[0,0]*inv_rho_gf
 outfile = open("pytov.dat","w")
 for i in range(isurf):
     rline = "%18.9E %18.9E %18.9E %18.9E\n" % (rad[i]*inv_length_gf,\
