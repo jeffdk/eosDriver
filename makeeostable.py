c0e95e47b06d31fe163d7db9d8fe0a4ddcca6cb8
Christian D. Ott <cott@tapir.caltech.edu>
2013-03-07 06:17:24 -0800

    * makeeostable.py: read/write out eos table for s=const, ye=const case

--- makeeostable.py
+++ makeeostable.py
@@ -2,7 +2,7 @@ import sys
 from units import *
 from numpy import linspace, zeros, log10
 from eosDriver import eosDriver
-
+import numpy
 
 def makeeostable(nrhos,rhomin,rhomax,myeos,mytype,par1,par2):
     assert isinstance(myeos, eosDriver)
@@ -62,33 +62,49 @@ def makeeostable(nrhos,rhomin,rhomax,myeos,mytype,par1,par2):
         entropy = par1
         
         energy_shift = myeos.h5file['energy_shift'][0]
-        
-        for i in range(nrhos):
-            temp = 0.0
-            temp = \
-                myeos.getTemperatureFromQuantityTYe({'rho': 10.0**logrhos[i],
-                                                     'ye': ye,
-                                                     'temp': temp}, \
-                                                        'entropy', entropy)
-            
 
-            myeos.setState({'rho': 10.0**logrhos[i],
-                            'ye' : ye,
-                            'temp': temp})
+        eostablename = "eostable_ye=%06.3f_s=%06.3f.dat" % (par2,par1)
+        print eostablename
+        try: 
+            eosin = (numpy.loadtxt(eostablename))
+            eostable[:,0] = eosin[:,1]
+            eostable[:,1] = eosin[:,2]
             
-            (press,eps) = myeos.query(['logpress','logenergy'])
-            # convert units
-            eostable[i,0] = log10(10.0**press * press_gf)
-            eostable[i,1] = log10(10.0**eps * eps_gf)
+        except IOError:
+            for i in range(nrhos):
+                temp = 0.0
+                temp = myeos.getTemperatureFromQuantityTYe({'rho': 10.0**logrhos[i],
+                                                         'ye': ye,
+                                                         'temp': temp},'entropy', entropy)
+                myeos.setState({'rho': 10.0**logrhos[i],
+                                'ye' : ye,
+                                'temp': temp})
+                
+                (press,eps) = myeos.query(['logpress','logenergy'])
+                # convert units
+                eostable[i,0] = log10(10.0**press * press_gf)
+                eostable[i,1] = log10(10.0**eps * eps_gf)
 
-            print "Making EOS: %15.6E %15.6E %15.6E" % (10.0**logrhos[i],temp,ye)
+                print "Making EOS: %15.6E %15.6E %15.6E" % (10.0**logrhos[i],temp,ye)
+
 
         energy_shift = energy_shift*eps_gf
+
+        # write EOS table
+        eosfile=open(eostablename,"w")
+        for i in range(len(eostable[:,0])):
+            sline = "%15.6E %15.6E %15.6E\n" % \
+                (logrhos[i],eostable[i,0],eostable[i,1])
+            eosfile.write(sline)
+        eosfile.close()
+
 
     elif(mytype == 'fixed_entropy_betaeq'):
 
         energy_shift = myeos.h5file['energy_shift'][0]
         entropy = par1
+
+
         for i in range(nrhos):
             (ye,temp) = \
                 myeos.setConstQuantityAndBetaEqState({'rho': 10.0**logrhos[i]},\
