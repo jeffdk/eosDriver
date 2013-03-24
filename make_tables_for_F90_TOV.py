6d2980191b1d88e7d810c3f0dc1bee2ad12a0f8a
Christian Ott <cott@bethe.tapir.caltech.edu>
2013-03-24 09:57:13 -0700

    * updated code for eos tables

--- make_tables_for_F90_TOV.py
+++ make_tables_for_F90_TOV.py
@@ -8,8 +8,19 @@ from tov import *
 
 #define EOSs
 
-EOSlist = [ ["ls220","LS220_234r_136t_50y_analmu_20091212_SVNr26.h5"], 
-            ["shen", "HShenEOS_rho234_temp136_ye50_version2.0_20120706.h5"]   
+EOSlist = [ ["LS220","LS220_234r_136t_50y_analmu_20091212_SVNr26.h5"], 
+            ["HShen", "HShenEOS_rho220_temp180_ye65_version_1.1_20120817.h5"],
+            ["GShenFSU2.1",
+             "GShenFSU_2.1EOS_rho280_temp180_ye52_version_1.1_20120824.h5"],
+            ["GShenNL3",
+             "GShen_NL3EOS_rho280_temp180_ye52_version_1.1_20120817.h5"],
+            ["Hempel_DD2",
+             "Hempel_DD2EOS_rho234_temp180_ye60_version_1.1_20120817.h5"],
+            ["Hempel_SFHo",
+             "Hempel_SFHoEOS_rho222_temp180_ye60_version_1.1_20120817.h5"],
+            ["Hempel_SFHx",
+             "Hempel_SFHxEOS_rho234_temp180_ye60_version_1.1_20120817.h5"],
+            ["LS375","LS375_234r_136t_50y_analmu_20091212_SVNr26.h5"],
           ]
 
 yes = [0.1,0.15]
@@ -25,21 +36,22 @@ rhomin = 1.0e6
 rhomax = 8.0e15
 nrhos = 600
 
-for ieos in range(len(EOSlist)):
-    myeos = eosDriver(EOSlist[ieos][1])
-    for ii in range(len(temps)):
-        for jj in range(len(yes)):
-            mytype = "fixed_ye_temp"
-            par1 = temps[ii]
-            par2 = yes[jj]
-            print "T = %5.2f, Y_e = %5.2f" % (par1,par2)
-            print "Preparing EOS table: ",mytype
-            makeeostable.makeeostable(\
-                nrhos,rhomin,rhomax,myeos,EOSlist[ieos][0],\
-                    mytype,par1,par2)
+def fixed_ye_temp(EOSlist,temps,yes):
+    for ieos in range(len(EOSlist)):
+        myeos = eosDriver(EOSlist[ieos][1])
+        for ii in range(len(temps)):
+            for jj in range(len(yes)):
+                mytype = "fixed_ye_temp"
+                par1 = temps[ii]
+                par2 = yes[jj]
+                print "T = %5.2f, Y_e = %5.2f" % (par1,par2)
+                print "Preparing EOS table: ",EOSlist[ieos][0], mytype
+                makeeostable.makeeostable(\
+                    nrhos,rhomin,rhomax,myeos,EOSlist[ieos][0],\
+                        mytype,par1,par2)
 
-    del myeos
+        del myeos
     
     
-    
+fixed_ye_temp(EOSlist,temps,yes)
 
