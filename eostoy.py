271dd8a704a66eff2c69f90805cc5b166e048f26
Christian D. Ott <cott@tapir.caltech.edu>
2013-04-08 15:49:00 -0700

    implement expression for neutrino pressure

--- eostoy.py
+++ eostoy.py
@@ -2,7 +2,7 @@
 
 import sys
 from units import *
-from numpy import linspace, zeros, log10, pi, sqrt
+from numpy import linspace, zeros, log10, pi, sqrt, exp
 from eosDriver import eosDriver
 import makeeostable
 
@@ -15,6 +15,7 @@ kb_erg = 1.380658e-16
 avo = 6.0221367e23
 hc_mevcm = 1.97326966e-11*2.0*pi
 pi = 3.14159265358979e0
+mev_to_erg = 1.60217733e-6
 
 def get_ynu(eta,rho,temp):
     
@@ -23,7 +24,13 @@ def get_ynu(eta,rho,temp):
 
     return xynu
 
-rho = 1.0e15
+def get_pnu(eta,temp,rho,rhotrap):
+    pnu =  mev_to_erg * 4.0*pi/3.0 * (temp**4/hc_mevcm**3) * \
+        (21.0*pi**4 / 60.0 + 0.5*eta**2 * \
+             (pi**2 + 0.5*eta**2)) * exp(-rhotrap/rho)
+    return pnu
+
+rho = 1.0e14
 ylep = 0.1
 
 temp = 0.1
@@ -31,7 +38,7 @@ ylep = myeos.setBetaEqState({'rho': rho,
                            'temp': temp})
 print "initial Y_lep", ylep
 ye = ylep
-temp = 20.0
+temp = 30.0
 
 count = 0
 prec = 1.0e-6
@@ -83,6 +90,14 @@ ye = ye1
 
 print count,ye,F
 
+myeos.setState({'rho': rho,
+                'ye': ye,
+                'temp': temp})
+
+press = 10.0e0**myeos.query('logpress')
+
+print eta, press, get_pnu(eta,temp,rho,10.0**12.5)
+
 
 
 sys.exit()
@@ -117,7 +132,7 @@ energy_shift = 0.0
 
 
 rho = 1.000139999E+15
-temp = 40.0
+temp = 10.0
 ye = 0.10
 
 myeos.setState({'rho': rho,
